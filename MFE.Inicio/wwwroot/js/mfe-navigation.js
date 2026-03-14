e5f84788cb72c2fc090215b67828d38b09464218
Ing César Rolando Ortiz Rangel <cesar.ortiz@ksp.com.mx>
2026-03-13 18:19:56 -0600

    fix: debbuging navigation

--- MFE.Inicio/wwwroot/js/mfe-navigation.js
+++ MFE.Inicio/wwwroot/js/mfe-navigation.js
@@ -1,5 +1,6 @@
 ﻿// wwwroot/js/mfe-navigation.js
 window.notifyShellNavigation = function (path) {
+    console.log('MFE enviando path al Shell:', path);
     window.parent.postMessage(
         { type: 'mfe-navigation', path: path },
         '*'
